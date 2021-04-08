2255ad6cb29240d98724b0cbd3ee0e81a3718505
jeroen-sebastiaan <79712259+jeroen-sebastiaan@users.noreply.github.com>
2021-04-08 14:20:59 +0200

    Create Opdracht024.js

---
+++ Opdracht024.js
@@ -0,0 +1,8 @@
+// Paint a wall
+
+const paint = function(side, color){
+    console.log("The " + side + "wall has been painted " + color + '.');
+};
+
+paint('north', 'red');
+paint('south-east', 'grey');
