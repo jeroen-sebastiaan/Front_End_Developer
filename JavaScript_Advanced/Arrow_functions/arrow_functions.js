e592d30a02e7fb21149b82b822c8d251ac6a9713
Jeroen Sebastiaan <sebastiaanjeroen@hotmail.com>
2021-04-16 17:20:45 +0200

    opdracht01JSAdvanced

--- JavaScript_Advanced/Arrow_functions/arrow_functions.js
+++ JavaScript_Advanced/Arrow_functions/arrow_functions.js
@@ -1,5 +1,14 @@
-const ikRockArrowFunctions = function () {
-    console.log("Joe, ik rock de arrow functions!");
-  };
-  
-  ikRockArrowFunctions()
+// Functie 1 
+const ikRockArrowFunctions = () => ikRockArrowFunctions;
+
+// Functie 2
+const fivePlusSeven = () =>  5 + 7;
+
+//Functie 3
+const myFunction = (a, b) => a + b; 
+
+//Functie 4
+const addFive = a => a + 5;
+
+// Functie 5
+const createObject = () => ({greeting: "Hoi"});
