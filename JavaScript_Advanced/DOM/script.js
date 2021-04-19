da23781117c190dd22fab38a64ea527a279dd5bb
Jeroen Sebastiaan <sebastiaanjeroen@hotmail.com>
2021-04-19 21:12:27 +0200

    opdracht manipulating DOM

---
+++ JavaScript_Advanced/DOM/script.js
@@ -0,0 +1,10 @@
+//  Toevoegen aan de DOM
+// Stap 1: const listElement = document.createElement("li")
+// Stap 2: listItem.classList.add("list-item-class");
+// Stap 3: const list = document.querySelector("ul");
+// Stap 4: list.appendChild(listItem);
+
+// Verwijderen elementen
+// Stap 1: Selecteer de parent.
+// Stap 2: Selecteer het kind dat je wil verwijderen.
+// Stap 3: Call parent.removeChild(child)
